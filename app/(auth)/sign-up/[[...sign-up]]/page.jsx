7672e8a249ae4d9579a91b6daaaefb034a69fd4f
tejasvi@appeneure <tejasvi@appeneure.com>
2025-03-22 23:18:03 +0530

    Add initial project structure with components, middleware, and configuration files

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SignUp } from '@clerk/nextjs'
+
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  )
+}
+
+export default SignUpPage;
