ecc3dc19e0b34e8f64331837648607c1a1585563
tejasvi <tejasviraj8686@gmail.com>
2025-04-05 23:07:18 +0530

    Update next.config.ts to include commented-out configuration for experimental features and image handling. Add new dependencies in package.json and package-lock.json for @google/generative-ai, @hookform/resolvers, react-hook-form, uuid, and zod. Integrate AddCarForm component into the Create Car page for improved functionality.

--- app/(admin)/admin/cars/create/page.tsx
+++ app/(admin)/admin/cars/create/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import AddCarForm from './_components/add-car-form'
 const page = () => {
   return (
     <div className='p-6'>
         <h1 className='text-2xl font-bold mb-6'>Create Car</h1>
+        <AddCarForm />
     </div>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,38 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+  
+//   experimental: {
+//     serverComponentsHmrCache: true,
+//   },
+
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "thctpckilrxnmiwoaxyu.supabase.co",
+        
+//       },
+//     ],
+//   },
+
+//   async headers() {
+//     return [
+//       {
+//         source: "/embed",
+//         headers: [
+//           {
+//             key: "Content-Security-Policy",
+//             value: "frame-src 'self' https://roadsidecoder.created.app;",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
