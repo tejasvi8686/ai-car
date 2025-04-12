5861b0d95d178acd4ddec13f0cdb162f0d1986c1
tejasvi <tejasvi@appenuere>
2025-04-12 18:31:41 +0530

    Add test drive booking functionality with user authentication, reservation management, and cancellation features. Implement test drive form and reservations page to enhance user experience. Introduce components for displaying and managing test drive bookings, including error handling and loading states.

--- app/waitlist/page.jsx
+++ app/waitlist/page.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+"use client";
 
-const WaitlistPage = () => {
-  return <div>WaitlistPage</div>;
-};
+import React from "react";
+import Head from "next/head";
 
-export default WailPage
+export default function WaitlistPage() {
+  return (
+    <>
+      <Head>
+        <title>Waitlist</title>
+      </Head>
+      <div className="mt-20 fixed w-full">
+        <div className="aspect-video w-full">Add iframe here</div>
+      </div>
+    </>
+  );
+}
